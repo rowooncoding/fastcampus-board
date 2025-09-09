fb62d02bf95217b9ca4adb0204b9b78a3e358b54
Rowoon <dev.rowoon@gmail.com>
2025-09-09 15:29:53 +0900

    mod: 코드 수정

--- board/src/main/java/com/codebene/board/config/WebConfiguration.java
+++ board/src/main/java/com/codebene/board/config/WebConfiguration.java
@@ -33,8 +33,7 @@ public class WebConfiguration {
     // 스프링 시큐리티 빈 설정
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(Customizer.withDefaults())
+        http.cors(Customizer.withDefaults())
                 .authorizeHttpRequests((requests) -> requests.anyRequest().authenticated()) // 모든 request에 대해서 인증처리
                 .sessionManagement(
                         (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // REST API에서는 session이 생성되지 않도록
