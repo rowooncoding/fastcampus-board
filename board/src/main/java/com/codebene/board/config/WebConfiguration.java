36ea92c8d9935068d88300f2460a34900dcaa532
Rowoon <dev.rowoon@gmail.com>
2025-09-09 15:09:53 +0900

    bugfix: cors 설정 추가

---
+++ board/src/main/java/com/codebene/board/config/WebConfiguration.java
@@ -0,0 +1,46 @@
+package com.codebene.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class WebConfiguration {
+
+    // cors bean 설정
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/v1/**", configuration);
+
+        return source;
+    }
+
+    // 스프링 시큐리티 빈 설정
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests((requests) -> requests.anyRequest().authenticated()) // 모든 request에 대해서 인증처리
+                .sessionManagement(
+                        (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // REST API에서는 session이 생성되지 않도록
+                .csrf(CsrfConfigurer::disable) // csrf 검증에 대한것은 사용하지 않음
+                .httpBasic(Customizer.withDefaults()); // basic auth 사용(추후 삭제)
+
+        return http.build();
+    }
+}
