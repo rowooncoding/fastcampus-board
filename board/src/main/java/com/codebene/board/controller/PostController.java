9c331641a209c165a3538c1f541112363462e80c
Rowoon <dev.rowoon@gmail.com>
2025-09-09 15:57:52 +0900

    chore: 로깅 처리

--- board/src/main/java/com/codebene/board/controller/PostController.java
+++ board/src/main/java/com/codebene/board/controller/PostController.java
@@ -5,6 +5,7 @@ import com.codebene.board.model.PostPatchRequestBody;
 import com.codebene.board.model.PostPostRequestBody;
 import com.codebene.board.service.PostService;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/posts")
+@Slf4j
 public class PostController {
 
     private final PostService postService;
@@ -23,6 +25,8 @@ public class PostController {
     // 게시글 여러건 조회
     @GetMapping
     public ResponseEntity<List<Post>> getPosts() {
+        log.info("GET /api/v1/posts");
+
         var posts = postService.getPosts();
 
         return ResponseEntity.ok(posts);
@@ -31,6 +35,7 @@ public class PostController {
     // 단건 게시물 조회
     @GetMapping("/{postId}")
     public ResponseEntity<Post> getPostByPostId(@PathVariable Long postId) {
+        log.info("GET /api/v1/posts/{}", postId);
 
         Post post = postService.getPostByPostId(postId);
 
@@ -41,6 +46,8 @@ public class PostController {
     // 게시물 생성
     @PostMapping
     public ResponseEntity<Post> createPost(@RequestBody PostPostRequestBody postPostRequestBody) {
+        log.info("POST /api/v1/posts");
+
         var post = postService.createPost(postPostRequestBody);
         return ResponseEntity.ok(post);
     }
@@ -48,6 +55,7 @@ public class PostController {
     // 게시물 수정
     @PatchMapping("/{postId}")
     public ResponseEntity<Post> updatePost(@PathVariable Long postId, @RequestBody PostPatchRequestBody postPatchRequestBody) {
+        log.info("PATCH /api/v1/posts/{}", postId);
 
         var post = postService.updatePost(postId, postPatchRequestBody);
         return ResponseEntity.ok(post);
@@ -56,6 +64,7 @@ public class PostController {
     // 게시물 삭제
     @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePost(@PathVariable Long postId) {
+        log.info("DELETE /api/v1/posts/{}", postId);
 
         postService.deletePost(postId);
         return ResponseEntity.noContent().build();
